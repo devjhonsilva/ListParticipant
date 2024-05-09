eb7ae809d9e5dd08426db68124f0a5d6334ec595
Jonatan Santos <jonatansilvasantos@outlook.com>
2024-05-09 20:34:06 -0300

    Feito função de adicionar e remover convidado usando useState

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { TextInput, Text, SafeAreaView, TouchableOpacity, View, FlatList, Alert } from 'react-native';
 
 import { styles } from './style';
@@ -6,19 +6,41 @@ import Participant from '../../components/participant';
 
 
 export default function Home() {
+  const [participantes, setParticipantes] = useState<string[]>([]);
+  const [participanteName, setParticipantesName] = useState('');
+
+
+
   const handleParticipantAdd = () => {
-    return (
-      console.log('Clicou em adicionar')
-    )
+    if(participantes.includes(participanteName)){
+        return Alert.alert('Adicionar participante', 'Deseja confirma a participação desse convidado?', [
+        {
+          text: 'Sim',
+          style: 'default'
+        },
+        {
+          text: 'Não',
+          style:'cancel'
+        }
+      ])
+    }
+
+    setParticipantes(prevState => [...prevState, participanteName])
+    setParticipantesName('');
   }
 
   const handleParticipantRemove = (name: string) => {
-    return (
-      console.log(`removeu participante ${name}`)
-    )
+    Alert.alert("Remover", `Deseja remover o participante ${name}`, [
+      {
+        text: 'Sim',
+        onPress: () => setParticipantes(prevState => prevState.filter(participantes => participantes !== name))
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ])
   }
-
-  const participantes = ['Jonatan', 'Mayara', 'Vitinho', 'Fabio', 'Marinalva', 'Rita', 'Josivaldo', 'Breno', 'Ailton', 'Carlos', 'Jõao', 'Felipe', 'Daniel']
 
 
   return (
@@ -28,9 +50,11 @@ export default function Home() {
 
       <View style={styles.form}>
         <TextInput
-          placeholder="Nome do covidado"
+          placeholder='Nome do covidado'
           style={styles.input}
-          placeholderTextColor="#000"
+          placeholderTextColor='#000'
+          onChangeText={(text) => setParticipantesName(text)}
+          value={participanteName}
         />
 
         <TouchableOpacity
@@ -48,7 +72,7 @@ export default function Home() {
           <Participant
             name={item}
             key={item}
-            onRemove={() => handleParticipantRemove}
+            onRemove={() =>handleParticipantRemove(item)}
           />
         )}
         showsVerticalScrollIndicator={false}
